153a86cc17dad8baafec0c3f491ca9d90fddc45b
Ayman Soultana <61703445+Hydropic@users.noreply.github.com>
2023-09-14 16:14:33 +0100

    updated db.py setup

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,1 +1,9 @@
-
+from motor.motor_asyncio import AsyncIOMotorClient 
+  
+client = AsyncIOMotorClient("mongodb://localhost:2", username="root", password="example") 
+  
+db = client.test 
+  
+collection_users = db.users
+collection_clothes = db.clothes
+collection_closets = db.closets
